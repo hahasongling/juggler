107be7b00dd40f7b6a8c9bbe3d4211b1d9885666
Songling Wang <wangsonglingchina@163.com>
2022-12-16 09:36:00 -0700

    juggler to do app

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/js/index.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
